599a51111afb8b396fc5211d70ef16ed10f1e9a9
Gaurang <gaurang.ma@gmail.com>
2017-10-31 17:03:02 +0530

    + React inroduction

---
+++ React introduction/src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1><p>GM Tuts .. !!</p>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
