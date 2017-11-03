70ded03621a5a64705cfe242380b9e91c5bf3a13
Gaurang <gaurang.ma@gmail.com>
2017-11-03 19:34:03 +0530

    + React import & export

---
+++ React import & export/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const App = () => {
+    return (
+      <div className="Test">
+        <h1>Hello, world!</h1><p>GM Tuts .. !!</p>
+      </div>
+    )
+  }
+
+
+export const App1 = () => {
+return (
+    <div className="Test">
+    <h1>Hello, world! App 1</h1><p>GM Tuts .. !!</p>
+    </div>
+)
+}
+
