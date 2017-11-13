6130691184fb183b9c8d406bff229d2bedaacdfa
Gaurang <gaurang.ma@gmail.com>
2017-11-13 12:17:34 +0530

    + React add state

---
+++ React add state/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Title = () => {
+  return <h1>GM Tuts .. !!</h1>
+}
+
+export class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      topics : ['React','Webpack','Angular','Angular2'],
+      topic : "",
+    }
+    this.inputChangeHandler = this.inputChangeHandler.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
+  }
+
+  inputChangeHandler(e) {
+    this.setState({
+      topic : e.target.value
+    })
+  }
+
+  onKeyPress(e) {
+    if(e.key === "Enter"){
+      this.setState({
+        topics : [...this.state.topics,this.state.topic] // spread operator
+      })
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Title />
+        <input type="text" value={this.state.topic} onChange={this.inputChangeHandler} onKeyPress={this.onKeyPress}/>
+        {this.state.topics.map((topic,index) => {
+          return <p key={index} >{topic}</p>
+        })}
+      </div>
+    )
+  }
+}
